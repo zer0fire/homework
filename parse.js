30569c8c0d12cd7148d78f8add060ba27aca92ab
zer0fire <1076671121@qq.com>
2022-08-17 01:18:30 +0800

    fix: 尝试解析 JSON

--- parse.js
+++ parse.js
@@ -1,12 +1,56 @@
-// let a = {
-//   a: 1,
-//   b: function () {
-
-//   }
+// {
+//   "A": 1,
+//   "B": 2
 // }
 
-// function parse() {
 
-// }
+`
+{
+  "A": 1,
+  "B": 2
+}
+`
+
+let token = []
+/**
+ * 
+ * @param {string} str 
+ */
+const global = {
+  index: 0
+}
+const stack = []
+function parse(str) {
+  
+  for(; index < str.length;) {
+    switcher(str[index])
+  }
+}
+
+
+function switcher(char) {
+  switch (char) {
+    case '{':
+      break;
+    case '}': 
+      break;
+    case '"': 
+      if (stack.length) {
+        state = 'keyStart'
+      } else {
+        k = stack.join()
+      }
+      break;
+    case ':':
+      state = 'value'
+      break;
+    case ',':
+      break;
+    case '': break;
+    default: 
+  }
+  global.index++
+}
+
 
 console.log(JSON.parse(JSON.stringify(a)))
