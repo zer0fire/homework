c7cf85cb98b2e221bfd8afc50c5eecab675bb234
wangmeng <wangmeng08@sparkedu.com>
2022-08-28 14:10:39 +0800

    fix: 增加 use

--- walk/__tests__/use.spec.js
+++ walk/__tests__/use.spec.js
@@ -1,5 +1,33 @@
 describe('walk 测试', () => {
-    test('单个节点', () => {
+  test('单个节点', () => {
+      // 建立 
+      // const ast = { a: [{ b: "2" }] }
+      // // 假函数，会记录调用次数
+      // const { walk, enter, leave } = require('../walk')
 
-    })
+      // // 执行一次创建一次 fn 的实例，特点是被调用的时候会被记录运行
+      // const mockEnter = jest.fn()
+      // const mockLeave = jest.fn()
+      
+      // walk(ast, { enter, leave })
+
+
+      // // 数组的情况
+      // // 这里是 enter
+      // let calls = mockEnter.mock.calls;
+
+      // expect(calls.length).toBe(3);
+      // expect(calls[0][0]).toEqual({ a: [{ b: "2" }] });
+      // expect(calls[1][0]).toEqual([{ b: "2" }]);
+      // expect(calls[2][0]).toEqual({ b: "2" });
+  
+      // calls = mockLeave.mock.calls;
+  
+      // expect(calls.length).toBe(3);
+      // expect(calls[0][0]).toEqual({ b: "2" });
+      // expect(calls[1][0]).toEqual([{ b: "2" }]);
+      // expect(calls[2][0]).toEqual({ a: [{ b: "2" }] });
+      
+      // 对象的情况
+  })
 })
