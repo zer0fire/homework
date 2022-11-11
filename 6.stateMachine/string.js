ecbd86df94ecc051badbd2c85ab70ff0408a8410
zer0fire <1076671121@qq.com>
2022-11-11 21:24:22 +0800

    fix: 不重复的优化

--- 6.stateMachine/string.js
+++ 6.stateMachine/string.js
@@ -8,18 +8,14 @@ function findSubstr(source, pattern) {
   // defg
   // 首先先找到 source 里 pattern 第一位的位置，然后循环，看后面的是不是也一致
   outer: for (let i = 0; i < source.length; i++) {
-    let result = true;
     inner: for (let j = 0; j < pattern.length; j++) {
+      //   如果 pattern 里没有重复的字母，那么，j 里面经过的子字符是可以直接跳过的，因此可以 i + j
       if (pattern[j] !== source[j + i]) {
-        // continue outer;
-        result = false;
-        break;
+        i += j - 1;
+        continue outer;
       }
     }
-    if (result) {
-      return i;
-    }
-    // return i;
+    return i;
   }
   return -1;
 }
