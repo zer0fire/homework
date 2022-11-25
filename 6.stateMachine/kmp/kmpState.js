3d7ed1f90ff467137210cee0b5a6171bec9a7be9
zer0fire <1076671121@qq.com>
2022-11-25 21:31:32 +0800

    fix: 半成品状态机

--- 6.stateMachine/kmp/kmpState.js
+++ 6.stateMachine/kmp/kmpState.js
@@ -1,53 +1,61 @@
-function end(input) {}
-
+function succeed() {
+  throw new Error("illegal succeed");
+}
 // i: ababcaabc
 // j:       ababcaabc
 //            ^
 // n: 001201120
 // n = [0, 0, 0, 1, 2, 0, 1, 1, 2, 0]
 
-function genNext(pattern) {
-  let next = [0, 0];
-  let i = 1,
-    j = 0;
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[j]) {
-      j++;
-      i++;
-      next[i] = j;
-    } else if (j === 0) {
-      i++;
-      next[i] = 0;
-    } else {
-      j = next[j];
-    }
+// ababc
+
+// abababcdef
+// ababc
+function start(input) {
+  if (input === "a") {
+    return findA;
+  } else {
+    return start;
   }
-  return next;
 }
 
-function search(source, pattern) {
-  let next = genNext(pattern);
-
-  let j = 0;
-  for (let i = 0; i < source.length; i++) {
-    // console.log("=======i", i, "======j", j);
-    if (source[i] === pattern[j]) {
-      j++;
-      if (j === pattern.length) {
-        return i - j + 1;
-      }
-    } else if (j !== 0) {
-      j = next[j];
-      i--;
-    }
+function findA(char) {
+  if (char === "b") {
+    return findB;
+  } else {
+    return start(char);
   }
-  return -1;
+}
+
+function findB(char) {
+  if (char === "a") {
+    return findC;
+  } else {
+    return start(char);
+  }
+}
+
+function findC(char) {
+  if (char === "b") {
+    return findD;
+  } else {
+    return start(char);
+  }
+}
+
+function findD(char) {
+  if (char === "c") {
+    return succeed;
+  } else {
+    return findB(char);
+  }
 }
 
 function strStr(source, needle) {
-  let state = genState(needle);
+  let state = start;
   for (let i = 0; i < source.length; i++) {
-    state = state(source[i], i);
+    console.log(state.name, i, needle[i]);
+    state = state(source[i]);
     if (state === succeed) {
       return i - (needle.length - 1);
     }
@@ -55,53 +63,23 @@ function strStr(source, needle) {
   return -1;
 }
 
-function genState(needle) {
-  function originState(condition, lastState, input) {
-    if (input === condition) {
-      return lastState;
-    } else {
-      return fail;
-    }
-  }
-  // 下一状态，转移条件
-  let state = succeed;
-  for (let j = needle.length - 1; j >= 0; j--) {
-    let lastState = state;
-    state = originState.bind(null, needle[j], lastState);
-  }
-  return state;
-}
-
-function start(input) {
-  if (input === "s") {
-    return findStrS;
-  } else {
-    return start;
-  }
-}
-
-function findStrS(input) {
-  if (input === "a") {
-    return findStrA;
-  } else {
-    return start;
-  }
-}
-
-function findStrA(input) {
-  if (input === "d") {
-    return succeed;
-  } else {
-    return start;
-  }
-}
-
-function succeed() {
-  throw new Error("illegal success call");
-}
+// function genState(needle) {
+//   function originState(condition, lastState, input) {
+//     if (input === condition) {
+//       return succeed;
+//     } else {
+//       return fail;
+//     }
+//   }
+//   // 下一状态，转移条件
+//   let state = succeed;
+//   for (let j = needle.length - 1; j >= 0; j--) {
+//     let lastState = state;
+//     state = originState.bind(null, needle[j], lastState);
+//   }
+//   return state;
+// }
 
 module.exports = {
   strStr,
-  genNext,
-  search,
 };
