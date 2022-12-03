8a55c95f5c6aee1a4fe3abee423d9bf0b3e8aab4
zer0fire <1076671121@qq.com>
2022-12-03 10:29:13 +0800

    fix: 用状态机过 kmp，但缺少合并状态和next

--- 6.stateMachine/__test__/kmp/kmpState.spec.js
+++ 6.stateMachine/__test__/kmp/kmpState.spec.js
@@ -2,10 +2,11 @@ const { strStr } = require("../../kmp/kmpState");
 describe("parse HTML", () => {
   // 串里找串
   test("not repeat", () => {
-    // expect(strStr("sadbutted", "sad")).toBe(0);
+    expect(strStr("asadbutted", "sad")).toBe(1);
   });
   test("repeat", () => {
-    // expect(strStr("mississippi", "issip")).toBe(0);
+    expect(strStr("asdfasdfsafabababafabababacasdf", "ababac")).toBe(21);
+    expect(strStr("mississippi", "issip")).toBe(4);
     expect(strStr("abababcdef", "ababc")).toBe(2);
   });
   // 需要 i 回退
--- 6.stateMachine/kmp/kmpState.js
+++ 6.stateMachine/kmp/kmpState.js
@@ -11,44 +11,157 @@ function succeed() {
 
 // abababcdef
 // ababc
-function start(input) {
-  if (input === "a") {
-    return findA;
-  } else {
-    return start;
-  }
-}
 
-function findA(char) {
-  if (char === "b") {
-    return findD;
-  } else {
-    return start(char);
-  }
-}
+// 从状态机的角度理解 kmp 或者其他动态规划题，好像就是限定状态不能无限转移而是有规律的转移，
+// 这个规律不一而足，有可能是根据一个公式或者公式的叠加，但是为什么要这么做却不清楚，只能说是目前按照这个规律去做事了
 
-function findC(char) {
-  if (char === "b") {
-    return findD;
-  } else {
-    return start(char);
-  }
-}
+// function find0(input) {
+//   if (input === "a") {
+//     return find1;
+//   } else {
+//     return find0;
+//   }
+// }
 
-function findD(char) {
-  if (char === "c") {
-    return succeed;
-  } else if (char === "a") {
-    return findC;
-  } else {
-    return findB(char);
-  }
-}
+// function find1(input) {
+//   if (input === "a") {
+//     return find1;
+//   } else if (input === "b") {
+//     return find2;
+//   } else {
+//     return find0;
+//   }
+// }
+
+// function find2(input) {
+//   if (input === "a") {
+//     return find3;
+//   } else {
+//     return find0;
+//   }
+// }
+
+// function find3(input) {
+//   if (input === "a") {
+//     return find1;
+//   } else if (input === "b") {
+//     return find4;
+//   } else {
+//     return find0;
+//   }
+// }
+
+// function find4(input) {
+//   if (input === "a") {
+//     return find5;
+//   } else {
+//     return find0;
+//   }
+// }
+
+// function find5(input) {
+//   if (input === "c") {
+//     return succeed;
+//   } else if (input === "b") {
+//     return find4;
+//   } else if (input === "a") {
+//     return find1;
+//   } else {
+//     return find0;
+//   }
+// }
+
+// function start(input) {
+//   if (input === "a") {
+//     return find1;
+//   } else {
+//     return start;
+//   }
+// }
+
+// function find1(char) {
+//   if (char === "b") {
+//     return find3;
+//   } else {
+//     return start(char);
+//   }
+// }
+
+// function find2(char) {
+//   if (char === "b") {
+//     return find3;
+//   } else {
+//     return start(char);
+//   }
+// }
+
+// function find3(char) {
+//   if (char === "c") {
+//     return succeed;
+//   } else if (char === "a") {
+//     return find2;
+//   } else {
+//     return start(char);
+//   }
+// }
+
+// { a: nextStateA, b: nextStateB }
 
 function strStr(source, needle) {
-  let state = start;
+  let start;
+  const allStates = [];
+  let next = [0, 0];
+
+  function findX(map, reconsume, n) {
+    return function findNext(char) {
+      // console.log("call", map);
+      if (map.has(char)) {
+        const v = map.get(char);
+        return allStates[v];
+      } else {
+        if (reconsume) {
+          return allStates[n](char);
+        } else {
+          return start;
+        }
+      }
+    };
+  }
+
+  function genState(needle) {
+    // 下一状态，转移条件
+    let state;
+    for (let j = 0; j < needle.length; j++) {
+      const map = new Map();
+      map.set(needle[j], j + 1);
+      state = findX(map, j !== 0, next[j]);
+      allStates.push(state);
+    }
+    start = allStates[0];
+    allStates.push(succeed);
+    return start;
+  }
+
+  function genNext(pattern) {
+    for (let i = 1, j = 0; i < pattern.length; i++) {
+      if (pattern[i] === pattern[j]) {
+        j++;
+        next[i + 1] = j;
+      } else if (j === 0) {
+        next[i + 1] = 0;
+      } else {
+        i--;
+        j = next[j];
+      }
+    }
+  }
+  genNext(needle);
+  let state = genState(needle);
+  // let state = find0;
+
   for (let i = 0; i < source.length; i++) {
-    console.log(state.name, i, needle[i]);
+    // console.log(state.name, i, source[i]);
+    // console.log(state, i, source[i]);
     state = state(source[i]);
     if (state === succeed) {
       return i - (needle.length - 1);
@@ -57,42 +170,18 @@ function strStr(source, needle) {
   return -1;
 }
 
-function findX(condition, nextState) {
-  return function findNext(char) {
-    if (char === condition) {
-      return nextState;
-    } else {
-      return start;
-    }
-  };
-}
-
-function genState(needle) {
-  function originState(condition, nextState, input) {
-    if (condition) {
-      return nextState;
-    } else {
-      return start;
-    }
-  }
-
-  function start(condition, nextState, input) {
-    if (input === condition) {
-      return nextState;
-    } else {
-      return start;
-    }
-  }
-
-  // 下一状态，转移条件
-  let state = succeed;
-  for (let j = needle.length - 1; j >= 0; j--) {
-    let lastState = state;
-    state = originState.bind(null, needle[j], start);
-  }
-  return state;
-}
+// 前面的已经算过了，就是说这个部分其实是重复的，如果比过后面的相等，前面的就不必再比了，
+// 如果不等，就和前面的再比
 
 module.exports = {
   strStr,
 };
+
+// 射线法相交，
+// 算点到外面点，是否能围成一个正确图形
+// 以当前点为中心做一个极坐标系，
+// 转角法
+// 海龙公式
+
+// 用状态机的方式过 leetcode
+// 一次循环就能搞定所有状态机和 next
