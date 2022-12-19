a5d0933773bfd2661c22481863343dd9a41b6c45
zer0fire <1076671121@qq.com>
2022-12-19 17:16:44 +0800

    fix: 改名和写 vue 编译器

--- extra/Vuejs/tokenize.js
+++ extra/Vuejs/tokenize.js
@@ -236,3 +236,34 @@ function transform(ast) {
 const tokens = parse(`<div><p>Vue</p><p>Template</p></div>`);
 transform(tokens);
 // console.log(tokens);
+
+function parseChildren(context, ancestors) {
+  let nodes = [];
+  const { mode, source } = context;
+
+  while (!isEnd(context, ancestors)) {
+    let node;
+    if (mode === TextModes.DATA || mode === TextModes.RCDATA) {
+      if (mode === TextModes.DATA && source[0] === "<") {
+        if (source[1] === "!") {
+          if (source.startsWith("<!--")) {
+            node = parseComment(context);
+          } else if (source.startsWith("<![CDATA[")) {
+            node = parseCDATA(context, ancestors);
+          }
+        } else if (source[1] === "/") {
+          // 结束标签
+        } else if (/[a-z]/i.test(source[1])) {
+          node = parseElement(context, ancestors);
+        }
+      } else if (source.startsWith("{{")) {
+        node = parseInterpolation(context);
+      }
+    }
+    if (!node) {
+      node = parseText(context);
+    }
+    nodes.push(node);
+  }
+  return nodes;
+}
