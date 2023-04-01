835f5927748bb11f468ed4335102a3a8277726ba
zer0fire <1076671121@qq.com>
2023-04-01 22:03:03 +0800

    fix: compiler

--- 10.VueCompiler/__test__/compiler.spec.js
+++ 10.VueCompiler/__test__/compiler.spec.js
@@ -2,6 +2,16 @@ import { parse } from "../src/compiler/parse";
 
 describe("compiler", () => {
   it("parse element", () => {
+    const template = "<div><img/></div>";
+    const ast = parse(template);
+    expect(ast[0]).toEqual({
+      tag: "div",
+      type: "Element",
+      children: [],
+      isUnary: false,
+    });
+  });
+  it("parse element unary", () => {
     const template = "<div></div>";
     const ast = parse(template);
     expect(ast[0]).toEqual({
@@ -11,4 +21,71 @@ describe("compiler", () => {
       isUnary: false,
     });
   });
+  it("parse element children", () => {
+    const template = "<div><p></p></div>";
+    const ast = parse(template);
+    expect(ast[0]).toEqual({
+      tag: "div",
+      type: "Element",
+      children: [],
+      isUnary: false,
+    });
+  });
+  // it("parse props and directive", () => {
+  //   const template = '<div id="foo" v-show="isShow"></div>';
+  //   const ast = parse(template);
+  //   expect(ast[0]).toEqual({
+  //     tag: "div",
+  //     type: "Element",
+  //     props: [
+  //       {
+  //         type: "Attribute",
+  //         name: "id",
+  //         value: "foo",
+  //       },
+  //       {
+  //         type: "Attribute",
+  //         name: "v-show",
+  //         value: "isShow",
+  //       },
+  //     ],
+  //     children: [],
+  //     isUnary: false,
+  //   });
+  // });
+  // it("parse plain text", () => {
+  //   const template = "<div>some text</div>";
+  //   const ast = parse(template);
+  //   expect(ast[0]).toEqual({
+  //     tag: "div",
+  //     type: "Element",
+  //     props: [],
+  //     children: [
+  //       {
+  //         type: "Text",
+  //         content: "some text",
+  //       },
+  //     ],
+  //     isUnary: false,
+  //   });
+  // });
+  // it("parse interpolation", () => {
+  //   const template = "<div>{{foo}}</div>";
+  //   const ast = parse(template);
+  //   expect(ast[0]).toEqual({
+  //     tag: "div",
+  //     type: "Element",
+  //     props: [],
+  //     children: [
+  //       {
+  //         type: "Interpolation",
+  //         content: {
+  //           type: "Expression",
+  //           content: "foo",
+  //         },
+  //       },
+  //     ],
+  //     isUnary: false,
+  //   });
+  // });
 });
--- 10.VueCompiler/src/compiler/parse.js
+++ 10.VueCompiler/src/compiler/parse.js
@@ -32,7 +32,8 @@ function isUppercase(char) {
  * @returns
  */
 function isAlphabet(char) {
-  return isLowercase(char) || isUppercase(char);
+  return char.match(/[a-zA-Z]/i);
+  // return isLowercase(char) || isUppercase(char);
   // return char !== "<" && char !== ">";
 }
 
@@ -46,6 +47,13 @@ export function parse(template) {
   // 上下文，携带参数，或者获取 this 等，保存当前状态，提供一些工具函数
   const context = {
     source: template,
+    // 消费，移动
+    advance(num) {
+      this.source = this.source.slice(num);
+    },
+    advanceSpace() {
+      // 吃空格
+    },
   };
 
   //   return [
@@ -99,92 +107,109 @@ function combineTag(nodes, retNodes = []) {
 
 /**
  *
- * @param {*} context
+ * @param {{ source: string }} context
  * @param {*} stack 判定节点内是否清空
  */
 function parseChildren(context, stack) {
   // 存储 ast 结果
-  const { source } = context;
-
   const nodes = [];
-  let tagObj = null;
 
-  function start(char) {
-    if (char === "<") {
-      return tagOpen;
-    } else {
-      return start;
-    }
-  }
-
-  function tagOpen(char) {
-    if (char === "/") {
-      return tagEnd;
-    } else if (isAlphabet(char)) {
-      return tagStart(char);
-    } else {
-      return start;
-    }
-  }
-
-  function tagStart(char) {
-    if (isAlphabet(char)) {
-      if (!tagObj) {
-        tagObj = {
-          type: "tagStart",
-          name: "",
-        };
+  // 结束条件
+  while (!isEnd(context, stack)) {
+    let node = null;
+    // '<div></div>'
+    // context.source 可改变，可迁移
+    // <div>{{}}</div>
+    if (context.source[0] === "<") {
+      // 小于号
+      // 状态 2 到 状态 3
+      if (isAlphabet(context.source[1])) {
+        // 处理当前的节点树 递归
+        node = parseElement(context, stack);
       }
-      tagObj.name += char;
-      return tagStart;
-    } else if (char === ">") {
-      nodes.push(tagObj);
-      tagObj = null;
-      return start;
-    } else {
-      return start;
+    } else if (context.source.startsWith("{{")) {
+      // 插值
     }
-  }
-
-  function tagEnd(char) {
-    if (isAlphabet(char)) {
-      if (!tagObj) {
-        tagObj = {
-          type: "tagEnd",
-          name: "",
-        };
-      }
-      tagObj.name += char;
-      return tagStart;
-    } else if (char === ">") {
-      nodes.push(tagObj);
-      tagObj = null;
-      return start;
-    } else {
-      return start;
+    if (!node) {
+      // 文本节点
+      parseText(context, stack);
     }
+    nodes.push(node);
   }
-
-  let machine = start;
-  let i = 0;
-  // 开启状态机，
-  while (source && i < source.length) {
-    // isEnd 结束条件 错误停止或者解析到最后停止
-    // < 开始
-    // a-z 标签
-    // {{ 插值
-    // > 结束
-    // parseElement 专门解决 a-z
-    // parseInterpolation 专门解决插值
-    // parseTextNode 遇到文本节点
-    machine = machine(source[i], context);
-    i++;
-  }
-  // [{ tagStart, div}, {tagEnd, div}]
-  // 返回 nodes
-  // console.log({
-  //   nodes,
-  //   ele: combineTag(nodes),
-  // });
-  return combineTag(nodes);
+  return nodes;
 }
+function isEnd(context, stack) {
+  if (!context.source) {
+    // source 没了，结束
+    return true;
+  }
+  // 判断闭合标签。配对情况，有结束标签就配对，
+  // stack 的最后一个元素和当前标签可以配对
+  // { tag: 'div', type: 'tagStart' }
+  const node = stack[stack.length - 1];
+  // </div>
+  if (node) {
+    // 配对结束了，结束
+    return context.source.startsWith(`</${node.tag}>`);
+  }
+}
+
+function parseElement(context, stack) {
+  // context.source 一部分转换成 { tag: 'div' }
+  // 开始、 <div>
+  const node = parseTag(context);
+  // props
+  if (!node.isUnary) {
+    // children、
+    stack.push(node);
+    // TODO:
+    // 处理 children
+    child = parseChildren(context, stack);
+
+    // 结束
+    parseTag(context, "end");
+    stack.pop();
+  }
+  return node;
+}
+// /^<([a-z][^\t\r\n\f />]*)/i
+// /^<\/([a-z][^\t\r\n\f />]*)/i
+function parseTag(context, type = "start") {
+  const startTagReg = /^<([a-z][^\t\r\n\f />]*)/i;
+  const endTagReg = /^<\/([a-z][^\t\r\n\f />]*)/i;
+  const pattern = type === "start" ? startTagReg : endTagReg;
+  const node = {
+    tag: "",
+    type: "Element",
+    isUnary: false,
+    children: [],
+  };
+  // ['<div>', 'div', index: 0, input: '<div>', groups: undefined]
+  const consumeTag = pattern.exec(context.source);
+  const consumeTagLen = consumeTag[0].length;
+  node.tag = consumeTag[1];
+  // context.source 操作，消费 <div>
+  context.advance(consumeTagLen);
+
+  parseAttribute();
+
+  if (context.source.startsWith("/>")) {
+    node.isUnary = true;
+    context.advance(2);
+  } else {
+    node.isUnary = false;
+    context.advance(1);
+  }
+  return node;
+}
+
+// 作业1 文本
+function parseText(context, stack) {}
+
+// 作业2 解析属性
+function parseAttribute(context) {
+  // 吃属性
+  // 单双引号，属性判断
+}
+
+// 作业3 解析 {{}}
