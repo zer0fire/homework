7addc86cd121a211b5f157bf6cf7707127e32435
zer0fire <1076671121@qq.com>
2023-04-06 18:56:46 +0800

    fix: 解决 test-case 中的问题

--- 10.VueCompiler/__test__/compiler.spec.js
+++ 10.VueCompiler/__test__/compiler.spec.js
@@ -1,83 +1,99 @@
 import { parse } from "../src/compiler/parse";
 
 describe("compiler", () => {
-  // it("parse element", () => {
-  //   const template = "<div><img/></div>";
-  //   const ast = parse(template);
-  //   expect(ast[0]).toEqual({
-  //     tag: "div",
-  //     type: "Element",
-  //     props: [],
-  //     children: [],
-  //     isUnary: false,
-  //   });
-  // });
-  // it("parse element unary", () => {
-  //   const template = "<div></div>";
-  //   const ast = parse(template);
-  //   expect(ast[0]).toEqual({
-  //     tag: "div",
-  //     type: "Element",
-  //     props: [],
-  //     children: [],
-  //     isUnary: false,
-  //   });
-  // });
-  // it("parse element children", () => {
-  //   const template = "<div><p></p></div>";
-  //   const ast = parse(template);
-  //   expect(ast[0]).toEqual({
-  //     tag: "div",
-  //     type: "Element",
-  //     props: [],
-  //     children: [],
-  //     isUnary: false,
-  //   });
-  // });
-  // it("parse props and directive", () => {
-  //   const template = '<div id="foo" v-show="isShow"></div>';
-  //   const ast = parse(template);
-  //   expect(ast[0]).toEqual({
-  //     tag: "div",
-  //     type: "Element",
-  //     props: [
-  //       {
-  //         type: "Attribute",
-  //         name: "id",
-  //         value: "foo",
-  //       },
-  //       {
-  //         type: "Attribute",
-  //         name: "v-show",
-  //         value: "isShow",
-  //       },
-  //     ],
-  //     children: [],
-  //     isUnary: false,
-  //   });
-  // });
-  // it("parse plain text", () => {
-  //   const template = '<div id="some">some text</div>';
-  //   const ast = parse(template);
-  //   expect(ast[0]).toEqual({
-  //     tag: "div",
-  //     type: "Element",
-  //     props: [
-  //       {
-  //         type: "Attribute",
-  //         name: "id",
-  //         value: "some",
-  //       },
-  //     ],
-  //     children: [
-  //       {
-  //         type: "Text",
-  //         content: "some text",
-  //       },
-  //     ],
-  //     isUnary: false,
-  //   });
-  // });
+  it("parse element", () => {
+    const template = "<div><img/></div>";
+    const ast = parse(template);
+    expect(ast[0]).toEqual({
+      tag: "div",
+      type: "Element",
+      props: [],
+      children: [
+        {
+          tag: "img",
+          type: "Element",
+          props: [],
+          children: [],
+          isUnary: true,
+        },
+      ],
+      isUnary: false,
+    });
+  });
+  it("parse element unary", () => {
+    const template = "<div></div>";
+    const ast = parse(template);
+    expect(ast[0]).toEqual({
+      tag: "div",
+      type: "Element",
+      props: [],
+      children: [],
+      isUnary: false,
+    });
+  });
+  it("parse element children", () => {
+    const template = "<div><p></p></div>";
+    const ast = parse(template);
+    expect(ast[0]).toEqual({
+      tag: "div",
+      type: "Element",
+      props: [],
+      children: [
+        {
+          tag: "p",
+          type: "Element",
+          props: [],
+          children: [],
+          isUnary: false,
+        },
+      ],
+      isUnary: false,
+    });
+  });
+  it("parse props and directive", () => {
+    const template = '<div id="foo" v-show="isShow"></div>';
+    const ast = parse(template);
+    expect(ast[0]).toEqual({
+      tag: "div",
+      type: "Element",
+      props: [
+        {
+          type: "Attribute",
+          name: "id",
+          value: "foo",
+        },
+        {
+          type: "Attribute",
+          name: "v-show",
+          value: "isShow",
+        },
+      ],
+      children: [],
+      isUnary: false,
+    });
+  });
+  it("parse plain text", () => {
+    const template = '<div id="some">some text</div>';
+    const ast = parse(template);
+    expect(ast[0]).toEqual({
+      tag: "div",
+      type: "Element",
+      props: [
+        {
+          type: "Attribute",
+          name: "id",
+          value: "some",
+        },
+      ],
+      children: [
+        {
+          type: "Text",
+          content: "some text",
+        },
+      ],
+      isUnary: false,
+    });
+  });
   it("parse interpolation", () => {
     const template = "<div>{{foo}}</div>";
     const ast = parse(template);
