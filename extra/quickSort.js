42ee38a5579bde1bc376c8110c66d92649b61d71
zer0fire <1076671121@qq.com>
2023-07-05 11:48:34 +0800

    fix: 解决问题

--- extra/quickSort.js
+++ extra/quickSort.js
@@ -10,40 +10,75 @@
 //     ary[j] = ca
 // }
 /**
- * 
- * @param {Array} ary 
- * @param {Number} start 
- * @param {Number} end 
+ *
+ * @param {Array} ary
+ * @param {Number} start
+ * @param {Number} end
  */
 function qs(ary, start = 0, end = ary.length - 1) {
-    if (end <= start) {
-        return ary
+  if (end <= start) {
+    return ary;
+  }
+  const range = Math.floor(Math.random() * (end - start));
+  const pIdx = start + range;
+  const pivot = ary[pIdx];
+  swap(ary, end, pIdx);
+  let i = -1,
+    j = 0;
+  while (j <= end - 1) {
+    if (ary[j] <= pivot) {
+      i++;
+      swap(ary, i, j);
     }
-    const range = Math.floor(Math.random() * (end - start))
-    const pIdx = start + range
-    const pivot = ary[pIdx]
-    swap(ary, end, pIdx)
-    let i = -1, j = 0
-    while(j <= end - 1) {
-        if (ary[j] <= pivot) {
-            i++
-            swap(ary, i, j)
-        }
-        j++
-    }
-    i++
-    swap(ary, end, i)
-    qs(ary, start, i - 1)
-    qs(ary, i + 1, end)
-    return ary
+    j++;
+  }
+  i++;
+  swap(ary, end, i);
+  qs(ary, start, i - 1);
+  qs(ary, i + 1, end);
+  return ary;
 }
 
 function genArray(n) {
-    return new Array(n).fill(0).map(() => (Math.random() * 10) | 0)
+  return new Array(n).fill(0).map(() => (Math.random() * 10) | 0);
 }
 
 function isSorted(ary) {
-    return ary.slice(1).every((it, index) => {
-        return ary[index] <= it
-    })
+  return ary.slice(1).every((it, index) => {
+    return ary[index] <= it;
+  });
+}
+
+function qs(ary, start = 0, end = ary.length - 1) {
+  if (start >= end) {
+    return ary;
+  }
+  const pivotIndex = ((Math.random() * (end - start)) | 0) + start;
+  const pivot = ary[pivotIndex];
+  console.log(pivot, pivotIndex, ary);
+  [ary[pivotIndex], ary[end]] = [ary[end], ary[pivotIndex]];
+  let i = -1,
+    j = 0;
+  while (j < end) {
+    if (ary[j] <= pivot) {
+      i++;
+      [ary[j], ary[i]] = [ary[i], ary[j]];
+    }
+    j++;
+  }
+  i++;
+  [ary[i], ary[end]] = [ary[end], ary[i]];
+  qs(ary, start, i - 1);
+  qs(ary, i + 1, end);
+
+  return ary;
+}
+
+function quickSort(ary, start = 0, end = ary.length - 1) {
+  if (start >= end) return ary;
+  const pivotIndex = ((Math.random() * (end - start)) | 0) + start;
+  const pivot = ary[pivotIndex];
+  let left = ary.filter((item) => item <= pivot);
+  let right = ary.sort((item) => item > pivot);
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
